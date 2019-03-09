313ff8ee2ee5bc6b37c771bd090f91e6c9334509
arunkumarry <arunkumar.ry1@gmail.com>
2019-03-09 23:04:25 +0530

    added blogs and static files

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     publish_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/', default='')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:75]
+
+    def pub_date(self):
+        return self.publish_date.strftime("%b %e %Y")
+
