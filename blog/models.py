78e013f5e841cf246013e1171ef62b3349311f66
arunkumarry <arunkumar.ry1@gmail.com>
2019-03-09 19:52:38 +0530

    added home page and jobs

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
+    publish_date = models.DateTimeField()
     body = models.TextField()
+    image = models.ImageField(upload_to='images/', default='')
